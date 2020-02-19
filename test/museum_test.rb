dad34fa19005139943cc049be323c490913ad653
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-19 09:58:51 -0700

    Write test_it_can_admit_patrons

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -47,4 +47,22 @@ class MuseumTest < Minitest::Test
     @patron_2.add_interest("IMAX")
     assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
+
+  def test_it_can_admit_patrons
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    assert_equal [@patron_1, @patron_2, @patron_3], @dmns.patrons
+  end
+
+  # def test_it_can_return_patrons_by_exhibit
+  #   @dmns.add_exhibit(@gems_and_minerals)
+  #   @dmns.add_exhibit(@dead_sea_scrolls)
+  #   @dmns.add_exhibit(@imax)
+  #
+  #   @patron_1.add_interest("Dead Sea Scrolls")
+  #   @patron_1.add_interest("Gems and Minerals")
+  #   @patron_2.add_interest("Dead Sea Scrolls")
+  #   @patron_3.add_interest("Dead Sea Scrolls")
+  # end
 end
