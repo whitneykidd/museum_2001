05b9697fd37e0ca1a6c993b81f4ca543a7a5e542
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-19 09:04:58 -0700

    Write setup and test_it_exists

---
+++ test/exhibit_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+
+
+
+
+class ExhibitTest < Minitest::Test
+  def setup
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  def test_it_exists
+    assert_instance_of Exhibit, @exhibit
+  end
+end
