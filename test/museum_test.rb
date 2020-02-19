0fc774cf9ea76e86b9540ea246307ed53d55e265
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-19 09:30:39 -0700

    Write test_it_can_recommend_exhibits

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -7,9 +7,13 @@ require './lib/museum'
 class MuseumTest < Minitest::Test
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
   end
 
   def test_it_exists
@@ -27,5 +31,19 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@imax)
     expected = [@gems_and_minerals, @dead_sea_scrolls, @imax]
     assert_equal expected, @dmns.exhibits
+  end
+
+  def test_it_can_recommend_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(@patron_1)
+
+    @patron_2.add_interest("IMAX")
+    assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
+
   end
 end
