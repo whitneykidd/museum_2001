cc8105afa18ff92f678b5610ff36bf5845a873ed
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-19 09:09:17 -0700

    Update syntax

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -2,9 +2,6 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/exhibit'
 
-
-
-
 class ExhibitTest < Minitest::Test
   def setup
     @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
